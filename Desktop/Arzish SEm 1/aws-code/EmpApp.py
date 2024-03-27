17071c53bda167653737566bcce6c34b0cdcaa1a
Arzish Maneckji <arzishmaneckji0@gmail.com>
2024-03-27 16:29:20 +0530

    LAST EmpApp

--- Desktop/Arzish SEm 1/aws-code/EmpApp.py
+++ Desktop/Arzish SEm 1/aws-code/EmpApp.py
@@ -125,4 +125,4 @@ def AddEmp():
     return render_template('AddEmpOutput.html', name=emp_name)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
